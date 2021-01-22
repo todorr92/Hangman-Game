e93062a2d47e3d8ce8fde709d64265081ba068da
unknown <todorovicslobodan10@gmail.com>
2021-01-22 12:45:00 +0000

    Added loop to load each image

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
-import os
+
 
 # initialize pygame
 pygame.init()
 # define size of game container
 WIDTH, HEIGHT = 800, 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 # set a game name to display
 pygame.display.set_caption("Hangman Game!")
+
+# load images
+images = []
+for i in range(7):
+    image = pygame.image.load(
+        r"C:\Users\Zver\Coding\Hangman Game\images\hangman" + str(i) + ".png")
+    images.append(image)
+    print(images)
+
 # frames per second
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+# setup game loop
 while run:
     clock.tick(FPS)
 
