6559bba5c82e57234314ce786d5c15c17e2bd22b
unknown <todorovicslobodan10@gmail.com>
2021-01-22 12:51:22 +0000

    Changed color of game window to white

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 # define size of game container
 WIDTH, HEIGHT = 800, 500
-win = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 # set a game name to display
 pygame.display.set_caption("Hangman Game!")
 
@@ -15,7 +15,9 @@ for i in range(7):
     image = pygame.image.load(
         r"C:\Users\Zver\Coding\Hangman Game\images\hangman" + str(i) + ".png")
     images.append(image)
-    print(images)
+
+# game variables
+hangman_status = 0
 
 # frames per second
 FPS = 60
@@ -25,6 +27,10 @@ run = True
 # setup game loop
 while run:
     clock.tick(FPS)
+
+    # change window color to white
+    window.fill((255, 255, 255))
+    pygame.display.update()
 
     # checking events
     for event in pygame.event.get():
